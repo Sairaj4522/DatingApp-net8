6900e5762c2b449fbebba04325f44b66397ae741
Sairaj Chavan <sairaj4522@gmail.com>
2025-01-30 18:24:23 +0530

    End of section 4 added authentication using Jwt

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,8 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class BaseApiController : ControllerBase {}
