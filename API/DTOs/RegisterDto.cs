ef143559e0959447f00f3d5d75a7f6be58ec8a24
Sairaj4522 <sairaj4522@gmail.com>
2025-04-16 13:18:39 +0530

    End of section 7 - Error handling using middleware in API and errorInterceptor in Angular

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,10 @@ public class RegisterDto
 {
     [Required]
     // [MaxLength(100)]
-    public required string Username { get; set; }
+    public string Username { get; set; }
     
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 8 characters")]
+    public string Password { get; set; }
 
 }
